5de3a8b3aac3eec1ea67aef6393186bbcc207543
Tijani Ebenezer <ezebina360@gmail.com>
2025-09-29 09:39:31 +0100

    update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from components.upload import render_uploader
 from components.history_download import render_history_download
 from components.chatui import render_chat
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
 
 st.set_page_config(page_title="AI Research Assistant", layout="wide")
